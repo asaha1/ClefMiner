29fbcf1538c2f40e009ea66ef745bb20a6fc9b31
Himangshu Ranjan Borah <himangshu.nits@gmail.com>
2016-04-18 02:46:33 -0400

    Modified some sound manager aspects.

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -1,38 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-// This Class is taken from unity tutorials.
+// This Class is designed similar to unity tutorials.
 public class SoundManager : MonoBehaviour 
 {
-	public AudioSource efxSource;                   //Drag a reference to the audio source which will play the sound effects.
-	public AudioSource musicSource;                 //Drag a reference to the audio source which will play the music.
-	public static SoundManager instance = null;     //Allows other scripts to call functions from SoundManager.             
-	public float lowPitchRange = .95f;              //The lowest a sound effect will be randomly pitched.
-	public float highPitchRange = 1.05f;            //The highest a sound effect will be randomly pitched.
+	public AudioSource efxSource;
+	public AudioSource musicSource;
+	public static SoundManager instance = null;
 
 
 	void Awake ()
 	{
-		//Check if there is already an instance of SoundManager
 		if (instance == null)
-			//if not, set it to this.
 			instance = this;
-		//If instance already exists:
 		else if (instance != this)
-			//Destroy this, this enforces our singleton pattern so there can only be one instance of SoundManager.
 			Destroy (gameObject);
-
-		//Set SoundManager to DontDestroyOnLoad so that it won't be destroyed when reloading our scene.
 		DontDestroyOnLoad (gameObject);
 	}
 
 
-	//Used to play single sound clips.
 	public void PlaySingle(AudioClip clip)
 	{
-		//Set the clip of our efxSource audio source to the clip passed in as a parameter.
 		efxSource.clip = clip;
-		//Play the clip.
 		efxSource.Play ();
 	}
 }
